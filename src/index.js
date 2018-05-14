3e043d82f99f0fb9a4773607e422c8a2d0464330
Daniel Mahon <danielmahon@users.noreply.github.com>
2018-05-14 13:25:16 -0400

    Simplify and add exported function to process frontmatter images
    Signed-off-by: Daniel Mahon <danielmahon@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ module.exports = (
   { files, markdownNode, markdownAST, pathPrefix, getNode, reporter },
   pluginOptions
 ) => {
-  const defaults = {
-    name: 'uploads',
-  };
+  const defaults = {};
 
   const options = _.defaults(pluginOptions, defaults);
 
@@ -25,7 +23,8 @@ module.exports = (
   // This will also allow the use of html image tags
   const rawHtmlNodes = select(markdownAST, `html`);
 
-  return Promise.all(
+  // Promise markdown images in body
+  Promise.all(
     // Simple because there is no nesting in markdown
     markdownImageNodes.map(
       node =>
@@ -35,22 +34,23 @@ module.exports = (
             return resolve();
           }
           let imagePath;
+          // See if there is a matching file path from gatsby-source-filesystem
           const imageNode = _.find(files, file => {
-            if (file.sourceInstanceName === options.name) {
-              imagePath = slash(path.join(file.dir, '..', node.url));
-              return file.absolutePath === imagePath;
-            }
+            imagePath = slash(path.join(file.dir, '..', node.url));
+            return file.absolutePath === imagePath;
           });
-
-          const parentNode = getNode(markdownNode.parent);
-          // Make the image src relative to its parent node
-          node.url = path.relative(parentNode.dir, imagePath);
-
+          // Return if we didn't find a match
+          if (!imageNode) return resolve();
+          // Get the markdown file's parent directory
+          const parentDirectory = getNode(markdownNode.parent).dir;
+          // Make the image src relative to the markdown file
+          node.url = path.relative(parentDirectory, imagePath);
+          // Return modified node
           return resolve(node);
         })
     )
   ).then(markdownImageNodes =>
-    // HTML image node stuff
+    // Process HTML images in markdown body
     Promise.all(
       // Complex because HTML nodes can contain multiple images
       rawHtmlNodes.map(
@@ -95,6 +95,8 @@ module.exports = (
                 }
               });
 
+              if (!imageNode) return resolve();
+
               const parentNode = getNode(markdownNode.parent);
               // Make the image src relative to its parent node
               thisImg.attr('src', path.relative(parentNode.dir, imagePath));
@@ -108,4 +110,43 @@ module.exports = (
       markdownImageNodes.concat(htmlImageNodes).filter(node => !!node)
     )
   );
+};
+
+const fileNodes = [];
+
+module.exports.fmImagesToRelative = node => {
+  fileNodes.push(node);
+  if (node.internal.type === `MarkdownRemark`) {
+    // Convert paths in frontmatter to relative
+    function makeRelative(value) {
+      if (_.isString(value) && path.isAbsolute(value)) {
+        let imagePath;
+        const foundImageNode = _.find(fileNodes, file => {
+          if (!file.dir) return;
+          imagePath = path.join(file.dir, '..', value);
+          return file.absolutePath === imagePath;
+        });
+        if (foundImageNode) {
+          return path.relative(
+            path.join(node.fileAbsolutePath, '..'),
+            imagePath
+          );
+        }
+      }
+      return value;
+    }
+    _.each(node.frontmatter, (value, key) => {
+      if (_.isArray(value)) {
+        node.frontmatter[key] = _.map(value, val => {
+          if (_.isObject(val)) {
+            return _.mapValues(val, val2 => makeRelative(val2));
+          } else {
+            return makeRelative(val);
+          }
+        });
+      } else {
+        node.frontmatter[key] = makeRelative(value);
+      }
+    });
+  }
 };
